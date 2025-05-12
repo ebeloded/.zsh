861946d88ebebf9e006b139eca37e0607f90f788
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2025-05-12 10:11:47 -0700

    Add AUTO_CD option for easier directory navigation

--- env.zsh
+++ env.zsh
@@ -1,6 +1,9 @@
 # ~/.zsh/env.zsh
 # Environment variables
 
+# Shell options
+setopt AUTO_CD  # Allow changing directories without 'cd'
+
 # Add your environment variable exports below
 
 export SVELTE_INSPECTOR_TOGGLE=alt-shift
