7edc49b6af22fff74a1da7bdc9c75047270f3374
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2025-09-04 09:51:37 -0700

    chore(env): set windsurf as default editor
    - Set VISUAL and EDITOR environment variables to windsurf
    - Minor formatting adjustment for consistency

--- env.zsh
+++ env.zsh
@@ -2,11 +2,22 @@
 # Environment variables
 
 # Shell options
-setopt AUTO_CD  # Allow changing directories without 'cd'
+setopt AUTO_CD # Allow changing directories without 'cd'
 
 # Add your environment variable exports below
 
+<<<<<<< HEAD
 # Editor configuration
 export EDITOR=cursor
 export VISUAL=cursor
 export BROWSER=arc
+=======
+# Core
+export VISUAL=windsurf
+export EDITOR=windsurf
+
+export SVELTE_INSPECTOR_TOGGLE=alt-shift
+export NVM_DIR="$HOME/.nvm"
+export BROWSER=arc
+export MODULAR_HOME="/Users/ebeloded/.modular"
+>>>>>>> fa5bc73 (chore(env): set windsurf as default editor)
