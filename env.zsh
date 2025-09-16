bcf602c4fa2c376f49161d9b1e70a066089dcfac
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2025-09-16 13:19:07 -0700

    Update env.zsh

--- env.zsh
+++ env.zsh
@@ -6,17 +6,7 @@ setopt AUTO_CD # Allow changing directories without 'cd'
 
 # Add your environment variable exports below
 
-<<<<<<< HEAD
 # Editor configuration
 export EDITOR=cursor
 export VISUAL=cursor
 export BROWSER=arc
-=======
-# Core
-export VISUAL=windsurf
-export EDITOR=windsurf
-
-export SVELTE_INSPECTOR_TOGGLE=alt-shift
-export BROWSER=arc
-export MODULAR_HOME="/Users/ebeloded/.modular"
->>>>>>> fa5bc73 (chore(env): set windsurf as default editor)
