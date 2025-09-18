75a3ec19f79f2b8236183414ee59b55d8817006c
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2025-09-18 02:01:35 -0700

    Update completions.zsh

--- completions.zsh
+++ completions.zsh
@@ -1,5 +1,5 @@
 # NVM bash completion
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Google Cloud SDK
 # if [ -f '/Users/ebeloded/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/ebeloded/google-cloud-sdk/completion.zsh.inc'; fi
